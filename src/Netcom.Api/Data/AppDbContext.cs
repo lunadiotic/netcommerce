840d209ff2e704c445817a3298e74a7132e7c1ec
ahmad hakim <ahmad.hakim@wartek.belajar.id>
2026-06-30 20:34:02 +0700

    create model dbcontext and connection

---
+++ src/Netcom.Api/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+using Netcom.Api.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Netcom.Api.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
